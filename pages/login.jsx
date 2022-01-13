c4e8782cc64a060c718a24c45fcb123e0b636aa0
robin-cavli <robin.chacko@cavliwireless.com>
2022-01-13 22:22:33 +0530

    myprofile changes

--- pages/login.jsx
+++ pages/login.jsx
@@ -5,28 +5,29 @@ import Link from "next/link";
 import nextConfig from "../next.config";
 import { useRouter } from "next/router";
 import Toast from "../components/Toast";
+import { useCookies } from "react-cookie"
 
 const Login = () => {
- 
+  const [cookie, setCookie] = useCookies(["user"])
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const [error, setError] = useState(false);
   const router = useRouter();
-  useEffect(() => {
-    
-    //const token = localStorage.getItem('user-token');
-    
-    // if(token) {
-    //  router.push("/");
-    // }
-  })
+ 
   const handleClick = async () => {
     try {
+       //handle API call to sign in here.
      let user= await axios.post(nextConfig.API_URL+"users/signin", {
         username,
         password,
       });
-      
+      //setting cookie
+      setCookie("user", JSON.stringify(user.data), {
+        path: "/",
+        maxAge: 3600, // Expires after 1hr
+        sameSite: true,
+      })
+
       localStorage.setItem('user-token', user.data.token);
       localStorage.setItem('first-name', user.data.firstName);
       localStorage.setItem('last-name', user.data.lastName);
--- pages/myprofile.jsx
+++ pages/myprofile.jsx
@@ -1,67 +1,47 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Login.module.css";
 import nextConfig from "../next.config";
 import Auth from "../components/Auth";
 import Toast from "../components/Toast";
 
-
-
-const Signup = () => {
-  let auth=Auth();
-  const [userDetails,setUserDetails]=useState({});  //for initial rendering
-  const [loading,setLoading]=useState(false);  
-
-  //for page fields
-  const [firstName, setFirstname] = useState(null);
-  const [lastName, setLastname] = useState(null);
-  const [userName, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
- 
-  const [password, setPassword] = useState(null);
-  const [error, setError] = useState(false);
-  const router = useRouter();
-  useEffect(async () => {
-    
-    const user_token = localStorage.getItem('user-token');
-    
-     console.log(auth);
-     if(auth.auth)
-     {
-      const user_id = localStorage.getItem('user-id');
-    
-      let user=await axios.get(nextConfig.API_URL+"users/user-details/"+user_id,{ 'headers': { 'authorization': user_token } });
-      
-     // setFirstname(user.data.data.firstName);
-      //initial rendering
-      setUserDetails((prevState) => ({
-       ...prevState,
-       ['firstName']:user.data.data.firstName ,
-       ['lastName']:user.data.data.lastName,
-       ['email']:user.data.data.email,
-       ['userName']:user.data.data.userName,
-    }));
-    setLoading(true);
-     }
-     else
-     {
-      
-     }
-   
-   
-    
+const Signup = ({ user }) => {
   
+  const router = useRouter();
+ 
+  useEffect(() => {
+
+    if(Object.keys(user).length === 0)
+    {
+      router.push("/login")
+    }
   })
-   
+  
+  const [userDetails, setUserDetails] = useState(user); //for initial rendering
+
+  const [error, setError] = useState(false);
+ 
+
+  // We need to spread the previous state and change the one we're targeting, so other data cannot be lost.
+  const handleChange = (e) => {
+    setUserDetails((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const handleClick = async () => {
     try {
-      await axios.post(nextConfig.API_URL+"users/update", {
-        firstName,
-        lastName,
-        userName,
-        email,
-        password,
+      const headers = {
+        'Content-Type': 'application/json',
+        'authorization': localStorage.getItem('user-token')
+      }
+      
+      await axios.post(nextConfig.API_URL + "users/update", 
+        userDetails
+      ,{
+        headers: headers
       });
       router.push("/");
     } catch (err) {
@@ -69,45 +49,42 @@ const Signup = () => {
     }
   };
 
-
   return (
-    loading &&
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1>Hangover Registration</h1>
         First name
         <input
           placeholder=" First name"
-          className={styles.input}  value={userDetails.firstName}
-          onChange={(e) => setFirstname(e.target.value)}
+          className={styles.input}
+          name="firstName"
+          value={userDetails.firstName}
+          onChange={handleChange}
         />
-          Last Name
+        Last Name
         <input
           placeholder="Last Name"
-          className={styles.input} value={userDetails.lastName}
-          onChange={(e) => setLastname(e.target.value)}
+          className={styles.input}
+          name="lastName"
+          value={userDetails.lastName}
+          onChange={handleChange}
         />
-        {console.log("last,",firstName)}
-          email
+        email
         <input
           placeholder="email"
-          className={styles.input} value={userDetails.email}
-          onChange={(e) => setEmail(e.target.value)}
+          className={styles.input}
+          name="email"
+          value={userDetails.email}
+          onChange={handleChange}
         />
         username
         <input
           placeholder="username"
-          className={styles.input} value={userDetails.userName}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        {/* password
-        <input
-          placeholder="password"
-          type="password"
           className={styles.input}
-          onChange={(e) => setPassword(e.target.value)}
-        /> */}
-       
+          name="userName"
+          value={userDetails.userName}
+          onChange={handleChange}
+        />
         <button onClick={handleClick} className={styles.button}>
           Update profile
         </button>
@@ -118,3 +95,32 @@ const Signup = () => {
 };
 
 export default Signup;
+
+//server api call before page load
+
+export const getServerSideProps = async (ctx) => {
+  let user = {};
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+  //console.log(JSON.parse(myCookie.user));
+  if (myCookie.user) {
+    let userDetails = JSON.parse(myCookie.user);
+
+    user = await axios.get(
+      nextConfig.API_URL + "users/user-details/" + userDetails._id,
+      { headers: { authorization: userDetails.token } }
+    );
+
+    user = user.data.data;
+  }
+
+  return {
+    props: {
+      user,
+    },
+  };
+};
