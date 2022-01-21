d2c9966e22699658447aa64f9a03691c6f9cc863
robinchacko246 <55970127+robinchacko246@users.noreply.github.com>
2022-01-21 11:16:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const {
 // This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
 module.exports = (phase) => {
 
- 
+  console.log(phase);
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
   const isDev = phase === PHASE_DEVELOPMENT_SERVER
   // when `next build` or `npm run build` is used
